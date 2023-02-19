0e37c93984fbebe806ee7595ee4c59d1eee6fbca
2382546457 <2382546457@qq.com>
2023-02-19 16:44:35 +0800

    教学计划与媒资文件绑定

---
+++ xuecheng-plus-content/xuecheng-plus-api/src/main/java/com/xuecheng/content/api/FreemarkerController.java
@@ -0,0 +1,20 @@
+package com.xuecheng.content.api;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author : 小何
+ * @Description :
+ * @date : 2023-02-18 19:14
+ */
+@Controller
+public class FreemarkerController {
+
+
+
+
+
+
+
+
+}
